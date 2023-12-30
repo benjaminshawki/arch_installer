c1208c93c7e78fcdbdaca261d4913f7513ae5eb6
benjaminshawki <benjaminshawki@gmail.com>
2023-12-30 02:49:36 +0100

    pacman and yay apps

---
+++ install_apps.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+name=$(cat /tmp/user_name)
+
+apps_path="/tmp/apps.csv"
+
+# Don't forget to replace "benjaminshawki" by the username of your Github account
+curl https://raw.githubusercontent.com/benjaminshawki\
+/arch_installer/main/apps.csv > $apps_path
+
+dialog --title "Welcome!" \
+--msgbox "Welcome to the install script for your apps and dotfiles!" \
+    10 60
+
+# Allow the user to select the group of packages they wish to install.
+apps=("essential" "Essentials" on
+      "network" "Network" on
+      "tools" "Nice tools to have (highly recommended)" on
+      "tmux" "Tmux" on
+      "notifier" "Notification tools" on
+      "git" "Git & git tools" on
+      "i3" "i3 wm" on
+      "zsh" "The Z-Shell (zsh)" on
+      "neovim" "Neovim" on
+      "alacritty" "Alacritty" on
+      "google-chrome" "Google Chrome" on
+      "js" "JavaScript tooling" on
+      "documents" "Documents tools" on
+
+dialog --checklist \
+"You can now choose what group of application you want to install. \n\n\
+You can select an option with SPACE and valid your choices with ENTER." \
+0 0 0 \
+"${apps[@]}" 2> app_choices
+choices=$(cat app_choices) && rm app_choices
+
+# Create a regex to only select the packages we want
+selection="^$(echo $choices | sed -e 's/ /,|^/g'),"
+lines=$(grep -E "$selection" "$apps_path")
+count=$(echo "$lines" | wc -l)
+packages=$(echo "$lines" | awk -F, {'print $2'})
+
+echo "$selection" "$lines" "$count" >> "/tmp/packages"
+
+pacman -Syu --noconfirm
+
+rm -f /tmp/aur_queue
+
+dialog --title "Let's go!" --msgbox \
+"The system will now install everything you need.\n\n\
+It will take some time.\n\n " \
+13 60
+
+c=0
+echo "$packages" | while read -r line; do
+    c=$(( "$c" + 1 ))
+
+    dialog --title "Arch Linux Installation" --infobox \
+    "Downloading and installing program $c out of $count: $line..." \
+    8 70
+
+    ((pacman --noconfirm --needed -S "$line" > /tmp/arch_install 2>&1) \
+    || echo "$line" >> /tmp/aur_queue) \
+    || echo "$line" >> /tmp/arch_install_failed
+
+    if [ "$line" = "zsh" ]; then
+        # Set Zsh as default terminal for our user
+        chsh -s "$(which zsh)" "$name"
+    fi
+
+    if [ "$line" = "networkmanager" ]; then
+        systemctl enable NetworkManager.service
+    fi
+done
+
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Don't forget to replace "benjaminshawki" by the username of your Github account
+curl https://raw.githubusercontent.com/benjaminshawki\
+/arch_installer/main/install_user.sh > /tmp/install_user.sh;
+
+# Switch user and run the final script
+sudo -u "$name" sh /tmp/install_user.sh
---
+++ install_user.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+mkdir -p "/home/$(whoami)/Documents"
+mkdir -p "/home/$(whoami)/Downloads"
+
+# Uncomment to set the keymap you want. Replace "fr" by your country code
+# localectl --no-convert set-x11-keymap fr
+
+# Function able to install any package from the AUR (needs the package names as arguments).
+aur_install() {
+    curl -O "https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz" \
+    && tar -xvf "$1.tar.gz" \
+    && cd "$1" \
+    && makepkg --noconfirm -si \
+    && cd - \
+    && rm -rf "$1" "$1.tar.gz"
+}
+
+aur_check() {
+    qm=$(pacman -Qm | awk '{print $1}')
+    for arg in "$@"
+    do
+        if [[ "$qm" != *"$arg"* ]]; then
+            yay --noconfirm -S "$arg" &>> /tmp/aur_install \
+                || aur_install "$arg" &>> /tmp/aur_install
+        fi
+    done
+}
+
+cd /tmp
+dialog --infobox "Installing \"Yay\", an AUR helper..." 10 60
+aur_check yay
+
+count=$(wc -l < /tmp/aur_queue)
+c=0
+
+cat /tmp/aur_queue | while read -r line
+do
+    c=$(( "$c" + 1 ))
+    dialog --infobox \
+    "AUR install - Downloading and installing program $c out of $count: $line..." \
+    10 60
+    aur_check "$line"
+done
+
+DOTFILES="/home/$(whoami)/dotfiles"
+if [ ! -d "$DOTFILES" ]; then
+    # Don't forget to replace benjaminshawki with your own username on Github
+    git clone https://github.com/benjaminshawki/dotfiles.git \
+    "$DOTFILES" >/dev/null
+fi
+
+source "$DOTFILES/zsh/.zshenv"
+cd "$DOTFILES" && bash install.sh
